3d591f57c5f767df1a2c355522f5dc5d385a45f0
Abdulmuhaymin althabit <s202275600@kfupm.edu.sa>
2026-03-16 22:11:48 +0300

    Solution

--- LeetCode/Ez/100SameTree.py
+++ LeetCode/Ez/100SameTree.py
@@ -12,33 +12,41 @@ class Solution(object):
         :type q: Optional[TreeNode]
         :rtype: bool
         """
-        #temp refrences 
+        # BFs
+        stackT1 = []
+        stackT2 = []
 
         #Check the root
-        if p.val != q.val:
+        if p == None and q == None:
+            return True
+        if (p != None and q == None) or (p == None and q != None):
             return False
-        tempP = p.left
-        tempQ = q.left
+        stackT1.append(p)
+        stackT2.append(q)
 
-        #Checking the leftSide  
-        while  tempP.left != None and tempQ.left != None :
-            if tempP.val != tempQ.val:
-                return False
-            tempP = tempP.left
-            tempQ = tempQ.left
-            if (tempP.left != None and tempQ.left == None) or (tempP.left == None and tempQ.left != None):
+        while (stackT1) and (stackT2) :
+            if (stackT1 and not stackT2) or (not stackT1 and stackT2):
                 return False 
-        
-        tempP = p.right
-        tempQ = q.right
-
-        #Checking the right
-        while  tempP.right != None and tempQ.right != None :
-            if tempP.val != tempQ.val:
-                return False
-            tempP = tempP.right
-            tempQ = tempQ.right
-            if (tempP.right != None and tempQ.right == None) or (tempP.right == None and tempQ.right != None):
-                return False
-        return True   
             
+            nodeT1 = stackT1.pop(0)
+            nodeT2 = stackT2.pop(0)
+            if nodeT1.val != nodeT2.val:
+                return False
+            
+            if nodeT1.left != None and nodeT2.left != None:
+                 
+                 stackT1.append(nodeT1.left)
+                 stackT2.append(nodeT2.left)
+            elif (nodeT1.left == None and nodeT2.left != None) or (nodeT1.left == None and nodeT2.left != None):
+                return False
+
+            if nodeT1.right != None and nodeT2.right != None:
+                 stackT1.append(nodeT1.right)
+                 stackT2.append(nodeT2.right)
+
+            elif (nodeT1.right == None and nodeT2.right != None) or (nodeT1.right == None and nodeT2.right != None):
+                return False
+        
+        return True
+
+        
