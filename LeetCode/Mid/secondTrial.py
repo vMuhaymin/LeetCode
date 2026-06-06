e0a1818a6fb811d89e59367a444e2801f84bb15c
vMuhaymin <Q9fd.67@gmail.com>
2026-06-07 02:47:20 +0300

    pre-final Solution

--- LeetCode/Mid/secondTrial.py
+++ LeetCode/Mid/secondTrial.py
@@ -9,7 +9,6 @@ def climbingLeaderboard(ranked, player):
             placement[ranked[i]] = placementRanked
             placementRanked +=1
 
-    
     playerStatus= []
     counter = 1
     done = False
@@ -22,12 +21,12 @@ def climbingLeaderboard(ranked, player):
         if(i == 0 and j== len(ranked)-1 ):
             playerStatus.append(placement[ranked[j]] + 1)
             print(f'The rank of player is {player[i]} = {ranked[j]} with rank of {placement[ranked[j]] + 1} ')
-            return playerStatus
+            return list(reversed(playerStatus))
         elif(i == 0 and j== len(ranked)):
             playerStatus.append(placement[ranked[j-1]] + 1)
-            return playerStatus
+            return list(reversed(playerStatus))
         if (i == -1 or j== len(ranked)):
-            return playerStatus
+            return list(reversed(playerStatus))
 
         if (player[i]>= ranked[j]):
             if(player[i]== ranked[j]):
@@ -45,34 +44,7 @@ def climbingLeaderboard(ranked, player):
             print(f'The rank of player is {player[i]} < {ranked[j]}')
             j +=1
             counter +=1
-    return playerStatus
-
-
-    # for i in range(len(player) - 1, -1 , -1):
-    #     done = False
-    #     score = player[i]
-    #     while (not done):
-    #         if counter > len(placement) :
-    #             playerStatus.append(counter)
-    #             done= True
-    #             print("LAST")
-    #         elif (score > ranked[counter] and counter == 0):
-    #             playerStatus.append(1)
-    #             done= True
-    #             print(f'First with score={ score} > {ranked[counter]}| Rank = {1}')
-    #         elif (score > ranked[counter] ):
-    #             playerStatus.append(counter)
-    #             done= True
-    #             print(f'Bigger than prev with curr score= { score} > {ranked[counter]} | Rank = {counter}')
-    #         elif (score == ranked[counter] ):
-    #             playerStatus.append(placement[ranked[counter]])
-    #             done= True
-    #             print(f'Equal with curr score= {ranked[counter]} | Rank = {counter}')
-
-            
-    #         counter += 1
-    return playerStatus     
-
+        
 # ranked = [100, 90,90,80]
 # player = [70,80,105]
 
@@ -81,13 +53,6 @@ def climbingLeaderboard(ranked, player):
 
 ranked = [100, 90,90,80,75,60]
 player = [50,65,77,90,102]
-
-
-
-
-
-
-
 
 playersRank = climbingLeaderboard(ranked, player)
 print('-------------------------------------------')
