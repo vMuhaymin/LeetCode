d2cabbdeb9e9bba5dbcf0d282f758f305fe8f40d
vMuhaymin <Q9fd.67@gmail.com>
2026-06-07 02:27:45 +0300

    Edge case before gotta go

--- LeetCode/Mid/secondTrial.py
+++ LeetCode/Mid/secondTrial.py
@@ -11,42 +11,69 @@ def climbingLeaderboard(ranked, player):
 
     
     playerStatus= []
-    counter = 0
-    for i in range(len(player) - 1, -1 , -1):
-        done = False
-        score = player[i]
-        while (not done):
-            if counter > len(placement) :
-                playerStatus.append(counter)
-                done= True
-                print("LAST")
-            elif (score > ranked[counter] and counter == 0):
-                playerStatus.append(1)
-                done= True
-                print(f'First with score={ score} > {ranked[counter]}| Rank = {1}')
-            elif (score > ranked[counter] ):
-                playerStatus.append(counter)
-                done= True
-                print(f'Bigger than prev with curr score= { score} > {ranked[counter]} | Rank = {counter}')
-            elif (score == ranked[counter] ):
-                playerStatus.append(placement[ranked[counter]])
-                done= True
-                print(f'Equal with curr score= {ranked[counter]} | Rank = {counter}')
-            elif (score < ranked[counter] and counter == 0):
-                playerStatus.append(placement[ranked[counter]+1])
-                print(f'LAST with curr score= {ranked[counter]} | Rank = {counter+1}')
-                done= True
-            counter += 1
+    counter = 1
+    done = False
+    i = len(player) - 1;
+    j= 0;
+    while(not done):
+        print(f'--------------------- ')
+        print(f'round the i ={i} and j ={j} ')
+        if (i == -1 or j== len(ranked)):
+            return playerStatus
+        if (player[i]>= ranked[j]):
+            if(player[i]== ranked[j]):
+                placement[ranked[j]]
+                playerStatus.append(placement[ranked[j]])
+                print(f'The rank of player is {player[i]} = {ranked[j]} with rank of {placement[ranked[j]]} ')
+                i -=1
+                j +=1
+            else:
+                playerStatus.append(placement[ranked[j]])
+                print(f'The rank of player is {player[i]} > {ranked[j]} with rank of {placement[ranked[j]]} ')
+                i -=1
+                counter +=1
+        else:
+            print(f'The rank of player is {player[i]} < {ranked[j]}')
+            j +=1
+            counter +=1
+    return playerStatus
+
+
+    # for i in range(len(player) - 1, -1 , -1):
+    #     done = False
+    #     score = player[i]
+    #     while (not done):
+    #         if counter > len(placement) :
+    #             playerStatus.append(counter)
+    #             done= True
+    #             print("LAST")
+    #         elif (score > ranked[counter] and counter == 0):
+    #             playerStatus.append(1)
+    #             done= True
+    #             print(f'First with score={ score} > {ranked[counter]}| Rank = {1}')
+    #         elif (score > ranked[counter] ):
+    #             playerStatus.append(counter)
+    #             done= True
+    #             print(f'Bigger than prev with curr score= { score} > {ranked[counter]} | Rank = {counter}')
+    #         elif (score == ranked[counter] ):
+    #             playerStatus.append(placement[ranked[counter]])
+    #             done= True
+    #             print(f'Equal with curr score= {ranked[counter]} | Rank = {counter}')
+
+            
+    #         counter += 1
     return playerStatus     
 
 # ranked = [100, 90,90,80]
 # player = [70,80,105]
 
+ranked = [100, 100,50,40,40,20,10]
+player = [5,25,50,120]
+
 # ranked = [100, 90,90,80,75,60]
 # player = [50,65,77,90,102]
 
-# ranked = [100, 100,50,40,40,20,10]
-# player = [5,25,50,120]
+
 
 
 
