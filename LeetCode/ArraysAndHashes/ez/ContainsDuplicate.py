f281f9279aa531c7e0eb9866059c06f31cb059dc
Abdulmuhaymin althabit <s202275600@kfupm.edu.sa>
2026-07-09 08:58:36 +0300

    Solution

--- LeetCode/ArraysAndHashes/ez/ContainsDuplicate.py
+++ LeetCode/ArraysAndHashes/ez/ContainsDuplicate.py
@@ -9,8 +9,6 @@ class Solution:
         for i in range(len(nums)-1):
             if nums[i] == nums[i+1]:
                 return True
-        if nums[len(nums)-1] == nums[len(nums)-2]:
-            return True
         return False
                        
         
