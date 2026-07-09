750637d5d125a95757f1b935cce39c072b3a4327
Abdulmuhaymin althabit <s202275600@kfupm.edu.sa>
2026-07-09 08:56:11 +0300

    Solution

--- LeetCode/ArraysAndHashes/ez/ContainsDuplicate.py
+++ LeetCode/ArraysAndHashes/ez/ContainsDuplicate.py
@@ -1,13 +1,15 @@
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
+        
         nums.sort()
+        
         if len(nums) <= 1:
             return False
         
         for i in range(nums-1):
             if nums[i] == nums[i+1]:
                 return True
-        if nums[len[nums]-1] == nums[len[nums]-2]:
+        if nums[len(nums)-1] == nums[len(nums)-2]:
             return True
         return False
                        
