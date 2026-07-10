f3fb7c1c9cb900e6fc72f3131e8987d1e199b8d9
vMuhaymin <Q9fd.67@gmail.com>
2026-07-10 21:31:50 +0300

    Half way through

---
+++ LeetCode/ArraysAndHashes/ez/midArrayProb/groupAnagrams.py
@@ -0,0 +1,38 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        groupedAnagrams = []
+
+        while len(strs) >= 1 :
+            anagrams = []
+            
+            seen = {}
+            word = strs.pop()
+            
+            for j in word:
+                if j in seen:
+                    seen[j] = seen.get(j) +1 
+                else:
+                    seen[j] = 1
+            anagrams.append(word)
+
+            for otherWord in strs:
+                anotherSeen = {}
+                for i in otherWord:
+                    if i in anotherSeen:
+                        anotherSeen[i] = anotherSeen.get(i) +1
+                    else:
+                        anotherSeen[i] = 1
+
+                if seen == anotherSeen:
+                    anagrams.append(otherWord)
+                    strs.remove(otherWord)
+
+            groupedAnagrams.append(anagrams)
+        
+        return groupedAnagrams
+
+
+sol = Solution()
+words = ["eat","tea","tan","ate","nat","bat"]
+print(f"The solution is {sol.groupAnagrams(words)}")
