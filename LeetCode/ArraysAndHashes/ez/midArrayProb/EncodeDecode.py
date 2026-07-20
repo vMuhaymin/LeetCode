45099592d7406b3a7c8408acc70ca28abde04d9b
Abdulmuhaymin althabit <s202275600@kfupm.edu.sa>
2026-07-20 22:16:00 +0300

    Sol

--- LeetCode/ArraysAndHashes/ez/midArrayProb/EncodeDecode.py
+++ LeetCode/ArraysAndHashes/ez/midArrayProb/EncodeDecode.py
@@ -1,23 +1,27 @@
 class Solution:
 
     def encode(self, strs: List[str]) -> str:
-        counter = len (strs)
-        while counter > 0:
-            word = strs.pop(0)
-            if counter != 1 :
-                word +=" "
+        encoded =""
+        for word in strs:
             for ch in word:
-                strs.append(str(ord(ch)))
-            counter-=1
-        return strs 
+                encoded+= str(ord(ch))
+                encoded += str('#')
+            encoded += str('!')
+            
+        return encoded
 
     def decode(self, s: str) -> List[str]:
         word= ""
-        while s:
-            cara = s.pop(0)
-            word += chr(int(cara))  
-        words = word.split(' ')
-        for word in words:
-            s.append(word)
-        return s 
+        letter  = ""
+        words = [] 
+        for ch in s:
+            if ch == "!":
+                words.append(word)
+                word = ""
+            elif ch == "#":
+                word += chr(int(letter))
+                letter= ""
+            else: 
+                letter += ch
+        return words
         
