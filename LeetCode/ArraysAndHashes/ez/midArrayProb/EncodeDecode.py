ec93818c4908731c099be50010250006844b9771
Abdulmuhaymin althabit <s202275600@kfupm.edu.sa>
2026-07-23 11:31:33 +0300

    Solution to file

--- LeetCode/ArraysAndHashes/ez/midArrayProb/EncodeDecode.py
+++ LeetCode/ArraysAndHashes/ez/midArrayProb/EncodeDecode.py
@@ -1,27 +1,36 @@
-class Solution:
-
-    def encode(self, strs: List[str]) -> str:
-        encoded =""
-        for word in strs:
-            for ch in word:
-                encoded+= str(ord(ch))
-                encoded += str('#')
-            encoded += str('!')
-            
-        return encoded
-
-    def decode(self, s: str) -> List[str]:
-        word= ""
-        letter  = ""
-        words = [] 
-        for ch in s:
-            if ch == "!":
-                words.append(word)
-                word = ""
-            elif ch == "#":
-                word += chr(int(letter))
-                letter= ""
-            else: 
-                letter += ch
-        return words
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        if len(nums)<= 1:
+            return 1
         
+        heapq.heapify(nums)
+        root = heapq.heappop(nums)
+        LC = {root}
+        highest = 1
+        while nums:
+            
+            secNum = heapq.heappop(nums)
+            difference = abs(root - secNum)
+            if (difference == 0 or difference == 1):
+                if difference ==1:
+                    print(f'the two num are {root} and { secNum}')
+                    LC.add(secNum)
+                    if len(LC) > highest:
+                        highest = len(LC)
+                    print(LC)
+                flag = False
+            else:
+                flag = True
+                print(f"The nums are {secNum} and {root} flag is = {flag}")
+                if flag:
+                    LC = {root}
+            root = secNum
+
+            
+        return highest
