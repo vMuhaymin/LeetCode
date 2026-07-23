414c4cb3c74a2db8e9e2c022c9db42c1805512cd
Abdulmuhaymin althabit <s202275600@kfupm.edu.sa>
2026-07-23 11:31:02 +0300

    Solution

--- LeetCode/ArraysAndHashes/ez/test.py
+++ LeetCode/ArraysAndHashes/ez/test.py
@@ -5,8 +5,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums)<= 1:
+        if not nums:
             return 0
+        if len(nums)<= 1:
+            return 1
         
         heapq.heapify(nums)
         root = heapq.heappop(nums)
@@ -16,33 +18,41 @@ class Solution(object):
             
             secNum = heapq.heappop(nums)
             difference = abs(root - secNum)
-            if ( difference == 0 or difference == 1):
+            if (difference == 0 or difference == 1):
                 if difference ==1:
                     print(f'the two num are {root} and { secNum}')
                     LC.add(secNum)
                     if len(LC) > highest:
                         highest = len(LC)
                     print(LC)
+                flag = False
             else:
-                LC = {}
-            if nums:
-                root = heapq.heappop(nums)
-                LC.add(root)
-                if not nums:
-                    difference = abs(root - secNum)
-                    if ( difference == 0 or difference == 1):
-                        print(f'the two num are {root} and { secNum}')
-                        if difference ==1:
-                            LC.add(secNum)
-                            if len(LC) > highest:
-                                highest = len(LC)
-                            print(LC)
+                flag = True
+                print(f"The nums are {secNum} and {root} flag is = {flag}")
+                if flag:
+                    LC = {root}
+            root = secNum
+            # if nums:
+            #     root = heapq.heappop(nums)
+            #     if flag:
+            #         LC = {root}
+            #     else:
+            #         LC.add(root)
+            #     if not nums:
+            #         difference = abs(root - secNum)
+            #         if ( difference == 0 or difference == 1):
+            #             print(f'the two num are {root} and { secNum}')
+            #             if difference ==1:
+            #                 LC.add(secNum)
+            #                 if len(LC) > highest:
+            #                     highest = len(LC)
+            #                 print(LC)
             
         return highest
 
 
 sol = Solution()
-nums =[1,0,1, 2]
+nums = [100,4,200,1,3,2]
 print(f"The num of consec is {sol.longestConsecutive(nums)}")
 
                 
