abc543d15a029aaf5770c0fb68779387b09e232a
Abdulmuhaymin althabit <s202275600@kfupm.edu.sa>
2026-07-27 09:54:42 +0300

    Sol to sudoko 9 by 9

--- LeetCode/ArraysAndHashes/ez/midArrayProb/Valid Sudoku.py
+++ LeetCode/ArraysAndHashes/ez/midArrayProb/Valid Sudoku.py
@@ -25,19 +25,38 @@ class Solution(object):
                         return False
                     else:
                         col.add(numCol)
-
             col = {0}
+
+        prev = 0
+        bCol = 3
+        bRow = 3
+        square = {0}
+        while bCol != 12 and bRow != 12:
+            for i in range(prev,bCol):
+                for j in range(prev,bRow):
+                    if board[i][j] != ".":
+                        num = int(board[i][j])
+                        if num in square:
+                            return False
+                        else:
+                            square.add(num)
+            
+            prev = bCol
+            bCol += 3 
+            bRow +=3
+            square = {0}
         
         return True
 
 
 
 sol = Solution()
-res = sol.isValidSudoku([["5","3",".",".","7",".",".",".","."]
+res = sol.isValidSudoku(
+[["5","3",".",".","7",".",".",".","."]
 ,["6",".",".","1","9","5",".",".","."]
 ,[".","9","8",".",".",".",".","6","."]
 ,["8",".",".",".","6",".",".",".","3"]
-,["4",".",".","8",".","3",".",".","1"]
+,["4",".",".","8","2","3",".",".","1"]
 ,["7",".",".",".","2",".",".",".","6"]
 ,[".","6",".",".",".",".","2","8","."]
 ,[".",".",".","4","1","9",".",".","5"]
