063357a0b4f74b79095f5e96a1ae51cd11f53bc3
Abdulmuhaymin althabit <s202275600@kfupm.edu.sa>
2026-07-28 09:39:19 +0300

    TwoPointersSol

--- LeetCode/ArraysAndHashes/ez/ValidPalindrome.py
+++ LeetCode/ArraysAndHashes/ez/ValidPalindrome.py
@@ -4,13 +4,32 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        if s.strip(" ") == "" :
+
+
+
+        if s.strip(" ") == "" or len(s) == 1:
             return True
-        s.strip(',:')
-    
+        s = s.lower()
 
+        word= ""
+        letters = {'a'}
 
+        #Removing all non-alphanumeric characters
+        for letter in "qwertyuiopsdfghjklzxcvbnm1234567890":
+            letters.add(letter)
+        for ch in s:
+            if ch in letters:
+                word += ch
 
+        if len(word) == 1:
+            return True
 
-        
-        
+        #2 Pointers
+        left = 0
+        right = len(word) - 1 
+        while left < right :
+            if word[left] != word[right]:
+                return False
+            left += 1
+            right -=1
+        return True
