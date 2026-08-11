9f73cfca5bfc8f6a5c3546462abe95b433771eef
vMuhaymin <Q9fd.67@gmail.com>
2026-08-11 14:21:15 +0300

    Better approach

--- LeetCode/ArraysAndHashes/test.py
+++ LeetCode/ArraysAndHashes/test.py
@@ -1,16 +1,20 @@
-class Solution(object):
-    def productExceptSelf(self, nums):
+import heapq
+class Solution:
+    def topKFrequent(self, nums, k: int):
+        occur = {}
+        for num in nums:
+            occur[num] = occur.get(num, 0) + 1
+            
+        heap = []
+        for key in occur:
+            heapq.heappush(heap, (occur[key], key)) # One item is O(logn), whole items (nlogn)
+            if len(heap) > k :
+                heapq.heappop(heap)
+        res = []
+        while heap:
+            res.append(heapq.heappop(heap)[1])
+        return res[::-1]
 
-        p = [1] * len(nums)
-        s = [1] * len(nums)
-
-
-        p = 1
-        for i in range(1, len(nums) - 1):
-            p = p[i - 1 ] * nums[i - 1]
-        for i in range(len(nums)-2 , -1, -1):
-            s[i] = s[i+1] * nums[i+1]
-        return [s[i] * p[i] for i in range(len(nums))]
 
 sol = Solution()
-print(f"The result is {sol.productExceptSelf([1,2,3,4])}")
+print(f"Output: {sol.topKFrequent([1,1,1,2,2,3], k = 2)}")
