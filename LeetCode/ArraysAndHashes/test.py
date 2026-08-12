ed5702a6f591a02bc1b28c9e20edbe190ad7affe
vMuhaymin <Q9fd.67@gmail.com>
2026-08-12 18:14:29 +0300

    Solution

--- LeetCode/ArraysAndHashes/test.py
+++ LeetCode/ArraysAndHashes/test.py
@@ -5,19 +5,17 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         res = []
+        nums = sorted(nums)
         for i in range(1,len(nums)-1):
             L = 0
-
             while L < i :
                 for j in range(i+1, len(nums)):
-                    if nums[i] + nums[L] + nums[j] == 0 :
-                        print(f"FOUND ! L = {nums[L]} i = {nums[i]} R = {nums[j]}")
-                        if [nums[L] ,nums[i] , nums[j]] not in res:
-                            res.append([nums[L] ,nums[i] , nums[j]])
-                L+=1                        
+                    if nums[i] + nums[L] + nums[j] == 0  and  [nums[L] ,nums[i] , nums[j]] not in res:
+                        res.append([nums[L] ,nums[i] , nums[j]])
+                L+=1
 
         return res
 
 sol = Solution()
-print(f"The result is : {sol.threeSum([0,1,1])}")
+print(f"The result is : {sol.threeSum([0,0,0,0])}")
 
--- LeetCode/TwoPointersANDStack/mid/ThreeSum.py
+++ LeetCode/TwoPointersANDStack/mid/ThreeSum.py
@@ -5,27 +5,17 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         res = []
+        nums = sorted(nums)
         for i in range(1,len(nums)-1):
             L = 0
             while L < i :
                 for j in range(i+1, len(nums)):
-                    if nums[i] + nums[L] + nums[j] == 0 :
+                    if nums[i] + nums[L] + nums[j] == 0  and  [nums[L] ,nums[i] , nums[j]] not in res:
                         res.append([nums[L] ,nums[i] , nums[j]])
                 L+=1
-        i = 0
-        while i < len(res):
-            left = sorted(res[i])
-            j = i + 1
-            while j < len(res):
-                right = sorted(res[j])
-                if left == right :
-                    res.remove(res[j])
-                    j+=1
-                else:
-                    j+=1
-            i +=1
 
         return res
+
 
 sol = Solution()
 print(f"The result is : {sol.threeSum([0,0,0,0])}")
