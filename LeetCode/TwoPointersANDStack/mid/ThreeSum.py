cdcdc0431b51e04780cf26a0a54e46e5f20e1314
vMuhaymin <Q9fd.67@gmail.com>
2026-08-12 17:49:30 +0300

    Solution

--- LeetCode/TwoPointersANDStack/mid/ThreeSum.py
+++ LeetCode/TwoPointersANDStack/mid/ThreeSum.py
@@ -10,11 +10,23 @@ class Solution(object):
             while L < i :
                 for j in range(i+1, len(nums)):
                     if nums[i] + nums[L] + nums[j] == 0 :
-                        if [nums[L] ,nums[i] , nums[j]] not in res:
-                            res.append([nums[L] ,nums[i] , nums[j]])
-                L+=1                        
+                        res.append([nums[L] ,nums[i] , nums[j]])
+                L+=1
+        i = 0
+        while i < len(res):
+            left = sorted(res[i])
+            j = i + 1
+            while j < len(res):
+                right = sorted(res[j])
+                if left == right :
+                    res.remove(res[j])
+                    j+=1
+                else:
+                    j+=1
+            i +=1
+
         return res
 
 sol = Solution()
-print(f"The result is : {sol.threeSum([0,1,1])}")
+print(f"The result is : {sol.threeSum([0,0,0,0])}")
 
