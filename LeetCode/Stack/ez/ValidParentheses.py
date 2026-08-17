c1ace59ebb20136684d337c5eb226a8bdf0f8e0e
vMuhaymin <Q9fd.67@gmail.com>
2026-08-18 00:52:30 +0300

    Start

--- LeetCode/Stack/ez/ValidParentheses.py
+++ LeetCode/Stack/ez/ValidParentheses.py
@@ -4,4 +4,22 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        
+        open = {'(', '{', '['}
+        close = {')', '}', ']'}
+
+        if not s:
+            return True
+
+        valid = []
+        for i in s:
+            if i in open:
+                valid.append(i)
+            else:
+                j = valid.pop()
+                if (i == '(' and j !=')' ) or (i == '{' and j !='}' ) or (i == '[' and j !=']' ):
+                    return False
+        return True
+
+sol = Solution()
+s ="([)]"
+print(f"Output: {sol.isValid(s)}")
