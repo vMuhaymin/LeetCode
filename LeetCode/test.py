7fea18822ac411b98c975cfac55b3fafc9981099
Abdulmuhaymin althabit <s202275600@kfupm.edu.sa>
2026-08-29 16:13:24 +0300

    Solution

--- LeetCode/test.py
+++ LeetCode/test.py
@@ -1,33 +1,45 @@
+import math
 class Solution(object):
-    def isValid(self, s):
+    def evalRPN(self, tokens):
         """
-        :type s: str
-        :rtype: bool
+        :type tokens: List[str]
+        :rtype: int
         """
-        open = {'(', '{', '['}
-        close = {')', '}', ']'}
-
-        if not s:
-            return True
-
-        valid = []
-        for i in s:
-            if i in open:
-                valid.append(i)
-                print(f'{i} is appended')
-            elif i in close and valid:
-                j = valid.pop()
-                print(f" i= {i} | j = {j}" )
-                if (j == '(' and i ==')' ) or (j == '{' and i =='}' ) or (j == '[' and i ==']' ):
-                    continue
-                else:
-                    return False
+        #Stack to keep track of the result
+        st = []
+        while tokens :
+            print(f"The stack is {st}")
+            item = tokens.pop(0)
+            if item not in {'+', '-','*','/'}:
+                st.append(int(item))
             else:
-                return False
-        if valid:
-            return False
-        return True
-
+                match item:
+                    case '+':
+                        second_operand = st.pop()
+                        first_operand = st.pop()
+                        st.append(first_operand + second_operand)
+                    case "-":
+                        second_operand = st.pop()
+                        first_operand = st.pop()
+                        st.append(first_operand  -  second_operand)
+                    case '*':
+                        second_operand = st.pop()
+                        first_operand = st.pop()
+                        st.append(first_operand * second_operand)
+                    case '/':
+                        second_operand = st.pop()
+                        first_operand = st.pop()
+                        num = first_operand //second_operand
+                        if num <= 0:
+                            num = math.ceil(first_operand /second_operand)
+                            st.append(num)
+                        else:
+                            st.append(first_operand //second_operand)
+        return st.pop()
 sol = Solution()
-s ="]]"
-print(f"Output: {sol.isValid(s)}")
+ex = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+print(f"Output: {sol.evalRPN(ex)}")
+
+
+
+
