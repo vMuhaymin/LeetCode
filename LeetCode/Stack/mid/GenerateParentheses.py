d34ae44d91b5fde1c505474177daad644c1e7512
vMuhaymin <Q9fd.67@gmail.com>
2026-09-13 21:51:00 +0300

    Solution

--- LeetCode/Stack/mid/GenerateParentheses.py
+++ LeetCode/Stack/mid/GenerateParentheses.py
@@ -4,4 +4,20 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
+        res = []
+        def treeGen(left, right , curr):
+            if len(curr) == 2 * n :
+                res.append(curr)
+            if left < n:
+                treeGen(left + 1 , right, curr+'(')
+            if right < left :
+                treeGen(left, right+1 , curr+')')
+        treeGen(0,0,'')
+        return res 
+
+
+
+
+        
+
         
